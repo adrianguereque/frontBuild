22e55c6975db998e99a1e27f7bfa389259e51c76
Adrian Guereque <aguereque96@gmail.com>
2025-04-08 18:29:44 -0600

    Maybe?

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Contacto from "./pages/contacto";
 import Usuarios from "./pages/Usuarios";
 
-// ProtectedRoute component that checks if the user is authenticated
-/*const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = localStorage.getItem("token"); // Check if a token exists (adjust based on your auth flow)
-
-  // If the user is not authenticated, redirect to the login page
-  if (!isAuthenticated) {
-    return <Navigate to="/" />;
-  }
-
-  return children; // Otherwise, render the protected route
-};*/
-
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        {/*<Route path="/" element={<Login />} />*/}
-        <Route path="/" element={<Home />} /> {/**Para la actividad que no haya autenticacion */}
-        {/* Recomiendo editar pages/Home para el build*/}
-
-        {/* Protected routes */} {/* Not protected for now */}
-        <Route 
-          path="/home" 
-          element={
-              <Home />
-          } 
-        />
-        <Route 
-          path="/contacto" 
-          element={
-              <Contacto />
-          } 
-        />
-        <Route 
-          path="/usuarios" 
-          element={
-              <Usuarios />
-          } 
-        />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/contacto" element={<Contacto />} />
+      <Route path="/usuarios" element={<Usuarios />} />
+    </Routes>
   );
 };
 
